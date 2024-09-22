eb2f87cdd1e72ad2f0c356d6207bed1e33825cdc
takasan1234 <takaya_san@icloud.com>
2024-09-22 19:33:30 +0900

    Initial Commit

---
+++ Duolingo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Duolingo
+//
+//  Created by 辻孝弥 on 2024/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Duolingo/DuolingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuolingoApp.swift
+//  Duolingo
+//
+//  Created by 辻孝弥 on 2024/09/22.
+//
+
+import SwiftUI
+
+@main
+struct DuolingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
